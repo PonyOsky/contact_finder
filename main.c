8727dcb2a20f027d27acad57e58bc19ada0bf07e
ChowChowCZ <90407450+PonyOsky@users.noreply.github.com>
2024-10-23 17:42:51 +0200

    getting argument as input

--- main.c
+++ main.c
@@ -6,32 +6,43 @@ void printOutContact(char *name, char *number){
     printf("%s\n", number);
 }
 
-int main(void) {
-    char buffer[101];
+int main(int argc, char *argv[]) {
+    char fileLine[101];
     int matches = 0;
     char phone[50];
     char name[50];
     bool isNumberLine = false;
-    while (fgets(buffer, sizeof(buffer), stdin)){
-        if(sizeof(buffer) <= 101){
+    char *argument = NULL;
+    if(argc == 2){
+        argument = argv[1];
+        printf("%s\n", argument);
+    }else {
+        while (fgets(fileLine, sizeof(fileLine), stdin)){
+            printf("%s", fileLine);
+        }
+    }
+    while (fgets(fileLine, sizeof(fileLine), stdin)){
+        if(sizeof(fileLine) <= 101){
             if(!isNumberLine){
                 int index = 0;
-                while(buffer[index] != '\n' && buffer[index] != '\0'){
-                    name[index] = buffer[index];
+                while(fileLine[index] != '\n' && fileLine[index] != '\0'){
+                    name[index] = fileLine[index];
                     index++;
                 }
                 name[index + 1] = '\0';
                 isNumberLine = true;
             }else{
                 int index = 0;
-                while(buffer[index] != '\n' && buffer[index] != '\0'){
-                    phone[index] = buffer[index];
+                while(fileLine[index] != '\n' && fileLine[index] != '\0'){
+                    phone[index] = fileLine[index];
                     index++;
                 }
                 isNumberLine = false;
                 phone[index + 1] = '\0';
 
             }
+        }else{
+            //error filed input
         }
     }
     if(matches == 0){
